00539f0c49d39539bacd951061080be1c51f0fdc
Axeris123 <axeris123@gmail.com>
2018-12-29 17:02:52 +0100

    removed console log

--- auth/verifyToken.js
+++ auth/verifyToken.js
@@ -3,7 +3,6 @@ const salt = 'WRbzwlS5Zrfu44Hca77z';
 
 let verifyToken = (req, res, next) => {
     const token = req.headers['authorization'].split(" ")[1];
-    console.log('middleware')
     if (!token)
         return res.status(403).send({auth: false, message: 'No token provided.'});
     jwt.verify(token, salt, (err, decoded) => {
